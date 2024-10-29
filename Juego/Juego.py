704110a97c1d0da949d7899f7f9ae2cbf7f5bc8c
JoSantana0005 <joseasantana05@gmail.com>
2024-10-28 22:41:28 -0400

    Se abre la ventana y se comienza la logica del juego

--- Juego/Juego.py
+++ Juego/Juego.py
@@ -7,5 +7,18 @@ import time
 py.init()
 #Ancho y Largo de la ventana del juego
 witdh,height = 700,500
-py.display.set_caption("Juego de la vida")
-py.display.set_mode((witdh,height))
+py.display.set_caption("Juego de la Vida")
+dimesiones = py.display.set_mode((witdh,height))
+#Color del fondo
+bg = 25,25,25
+dimesiones.fill(bg)
+#Abriendo la ventana
+while True:
+    py.display.flip()
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            exit()
+    #Logica para el juego
+        #Creando los cuadros
+        
