bbaf0711d004b67d7bb941f9e97499b78558094d
JoSantana0005 <joseasantana05@gmail.com>
2024-10-28 22:36:18 -0400

    Ancho y largo de la pantalla del juego

--- Juego/Juego.py
+++ Juego/Juego.py
@@ -1,3 +1,11 @@
 #Librerias a utilizar en el proyecto
 import pygame as py
 import numpy as np
+import time
+
+#Inicio pygame
+py.init()
+#Ancho y Largo de la ventana del juego
+witdh,height = 700,500
+py.display.set_caption("Juego de la vida")
+py.display.set_mode((witdh,height))
